13cf1f1f04814fb06eef1c5176d7db86516802c5
sainikm <isainikkhan@gmail.com>
2025-07-27 06:44:57 +0530

    Update main.go with a starter menu

--- main.go
+++ main.go
@@ -1,22 +1,16 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Welcome to Diary!")
+	fmt.Println("1. Insert new entry")
+	fmt.Println("2. View entries")
+	fmt.Println("3. Delete entry")
+	fmt.Println("4. Export entries")
+	fmt.Println("5. Exit")
 
-	fmt.Print("Enter your name: ")
-	name, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("Error reading input:", err)
-		return
-	}
-
-	name = strings.TrimSpace(name)
-	fmt.Printf("Hello %s\n", name)
+	// TODO: everything lol
 }
